7d452a89b393fc79948144d2604caea5b50a206b
Qasim <qasim.ali@ebryx.com>
2021-12-26 08:54:58 +0500

    separated frontend from backend

--- backend/src/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Project = require('./project.model');
-module.exports.Chat = require('./chat.model');
