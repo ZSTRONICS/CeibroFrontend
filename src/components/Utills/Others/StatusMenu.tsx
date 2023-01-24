eef84e8f7ff48798272d2108ea5e302349030787
aakbar2022 <aakbar@zstronics.com>
2023-01-24 19:23:39 +0500

    latest

--- src/components/Utills/Others/StatusMenu.tsx
+++ src/components/Utills/Others/StatusMenu.tsx
@@ -1,11 +1,5 @@
-import { Badge, Button, makeStyles, Typography } from "@material-ui/core";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import projectActions, {
-  getProjectsWithPagination,
-  getStatus
-} from "redux/action/project.action";
-import { RootState } from "redux/reducers";
+import { Badge, makeStyles, Typography } from "@material-ui/core";
+import { useState } from "react";
 import colors from "../../../assets/colors";
 import {
   getColorByStatus
@@ -15,13 +9,8 @@ import { getStyleClass } from "../../../config/styles.config";
 
 export const StatusMenu = (props: any) => {
   const { options } = props;
-  const [subTask, setSubTask]: any = useState(false)
-  const { getStatuses, drawerOpen } = useSelector(
-    (state: RootState) => state.project
-  );
 
   const classes = useStyles();
-  const dispatch = useDispatch();
 
   const [filter, setFilter] = useState<string>("All");
   // useEffect(() => {
