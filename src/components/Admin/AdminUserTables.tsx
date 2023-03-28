04a6e520beee4b17f7d155e3235f48631441f785
aakbar2022 <aakbar@zstronics.com>
2023-03-28 11:11:35 +0500

    updated table filters removed

--- src/components/Admin/AdminUserTables.tsx
+++ src/components/Admin/AdminUserTables.tsx
@@ -1,96 +1,136 @@
-import React from 'react'
-import { Avatar } from '@mui/material';
-import { momentdeDateFormat } from 'components/Utills/Globals/Common';
-import { GenericTable, MenuColumn } from './MuiTable/CTable';
+import React from "react";
+import { momentdeDateFormat } from "components/Utills/Globals/Common";
+import { GenericTable, MenuColumn } from "./MuiTable/CTable";
 import NameAvatar from "components/Utills/Others/NameAvatar";
-import CustomModal from 'components/Modal';
-import ProfileViewGlobal from 'components/Profile/ProfileViewGlobal';
-import { ConfirmDescriptionTag, DocumentNameTag } from 'components/CustomTags';
+import CustomModal from "components/Modal";
+import ProfileViewGlobal from "components/Profile/ProfileViewGlobal";
+import { ConfirmDescriptionTag, DocumentNameTag } from "components/CustomTags";
 
-function AdminUserTables(props:any) {
+function AdminUserTables(props: any) {
   const [open, setOpen] = React.useState<boolean>(false);
-  const [userDataLocal,setUserData]=React.useState<{}|any>({})
+  const [userDataLocal, setUserData] = React.useState<{} | any>({});
 
-  const handleToggle = (rowData:any) => {
+  const handleToggle = (rowData: any) => {
     setOpen((prev: boolean) => !prev);
-    rowData.firstName=  String(rowData.name).split(" ")[0]
-    rowData.surName=  String(rowData.name).split(" ")[1]
-    setUserData(rowData)
+    rowData.firstName = String(rowData.name).split(" ")[0];
+    rowData.surName = String(rowData.name).split(" ")[1];
+    setUserData(rowData);
   };
 
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 100,  },
-        {
-            field: 'profilePic',
-            headerName: '',
-            width: 120,
-            renderCell: (params:any) => {
-            // return  <Avatar alt={params.row.id} src={`${params.value}`} />
-            return    <NameAvatar
-            firstName={String(params.row.name).split(' ')[0]}
-            surName={String(params.row.name).split(' ')[1]}
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      sortable: false,
+      filterable: false,
+      width: 100,
+    },
+    {
+      field: "profilePic",
+      headerName: "",
+      sortable: false,
+      filterable: false,
+      width: 120,
+      renderCell: (params: any) => {
+        // return  <Avatar alt={params.row.id} src={`${params.value}`} />
+        return (
+          <NameAvatar
+            firstName={String(params.row.name).split(" ")[0]}
+            surName={String(params.row.name).split(" ")[1]}
             url={params.value}
             variant="circular"
           />
-            },
-          },
-        { field: 'name', headerName: 'Personal Details ', width: 380, renderCell: (params:any) => (
-            <div >
-              <DocumentNameTag>{params.value}</DocumentNameTag>
-              <ConfirmDescriptionTag sx={{color:'#0076C8'}}>{params.row.email}</ConfirmDescriptionTag>
-              <ConfirmDescriptionTag sx={{color:'#000000'}}>{params.row.phone}</ConfirmDescriptionTag>
-            </div>
-          )
-        },
-        { field: 'work', headerName: 'Work Details ', width: 380, renderCell: (params:any) => (
-            <div>
-              <DocumentNameTag>{params.value}</DocumentNameTag>
-              <ConfirmDescriptionTag sx={{color:'#0076C8'}}>{params.row.workEmail}</ConfirmDescriptionTag>
-              <ConfirmDescriptionTag sx={{color:'#000000'}}>{params.row.companyPhone}</ConfirmDescriptionTag>
-            </div>
-          )
-        },
-        { field: 'regDate', headerName: 'Reg Date', width: 150,  renderCell: (params:any) => (
-          <div>
-            <DocumentNameTag>{params.value}</DocumentNameTag>
-            </div>) },
-        MenuColumn({
-          field: 'action',
-          headerName: 'Action',
-          onClickEdit: (row) =>handleToggle(row),
-        }),
-      ];
-      const rows= props.users&&props.users.map((item:any, index:any)=>{
-        const  localDate = momentdeDateFormat(item.createdAt)
-        return{
-            id:index+1,
-            name:`${item.firstName} ${item.surName}`,
-            email: item.email,
-            regDate:localDate,
-            profilePic:item.profilePic,
-            phone:item.phone===""?"N/A":item.phone,
-            work:item.companyName===""?"N/A":item.companyName,
-            workEmail:item.workEmail===""?"N/A":item.workEmail,
-            companyPhone:item.companyPhone===""?"N/A":item.companyPhone,
-          }
-        })
-  
-  return (<>
-    <GenericTable rows={rows} columns={columns} />
-    {open===true&&<CustomModal
-        // maxWidth="450px"
-        showFullWidth={false}
-        showDivider={true}
-        showCloseBtn={true}
-        title={"Personal Details"}
-        isOpen={open}
-        handleClose={()=> setOpen((prev: boolean) => !prev)}
-        children={
-        <ProfileViewGlobal userData={userDataLocal}/>
-        }
-      />}
-  </>
-  )
+        );
+      },
+    },
+    {
+      field: "name",
+      headerName: "Personal Details ",
+      sortable: false,
+      filterable: false,
+      width: 380,
+      renderCell: (params: any) => (
+        <div>
+          <DocumentNameTag>{params.value}</DocumentNameTag>
+          <ConfirmDescriptionTag sx={{ color: "#0076C8" }}>
+            {params.row.email}
+          </ConfirmDescriptionTag>
+          <ConfirmDescriptionTag sx={{ color: "#000000" }}>
+            {params.row.phone}
+          </ConfirmDescriptionTag>
+        </div>
+      ),
+    },
+    {
+      field: "work",
+      headerName: "Work Details ",
+      sortable: false,
+      filterable: false,
+      width: 380,
+      renderCell: (params: any) => (
+        <div>
+          <DocumentNameTag>{params.value}</DocumentNameTag>
+          <ConfirmDescriptionTag sx={{ color: "#0076C8" }}>
+            {params.row.workEmail}
+          </ConfirmDescriptionTag>
+          <ConfirmDescriptionTag sx={{ color: "#000000" }}>
+            {params.row.companyPhone}
+          </ConfirmDescriptionTag>
+        </div>
+      ),
+    },
+    {
+      field: "regDate",
+      headerName: "Reg Date",
+      sortable: false,
+      filterable: false,
+      width: 150,
+      renderCell: (params: any) => (
+        <div>
+          <DocumentNameTag>{params.value}</DocumentNameTag>
+        </div>
+      ),
+    },
+    MenuColumn({
+      field: "action",
+      headerName: "Action",
+      onClickEdit: (row) => handleToggle(row),
+    }),
+  ];
+  const rows =
+    props.users &&
+    props.users.map((item: any, index: any) => {
+      const localDate = momentdeDateFormat(item.createdAt);
+      return {
+        id: index + 1,
+        name: `${item.firstName} ${item.surName}`,
+        email: item.email,
+        regDate: localDate,
+        profilePic: item.profilePic,
+        phone: item.phone === "" ? "N/A" : item.phone,
+        work: item.companyName === "" ? "N/A" : item.companyName,
+        workEmail: item.workEmail === "" ? "N/A" : item.workEmail,
+        companyPhone: item.companyPhone === "" ? "N/A" : item.companyPhone,
+      };
+    });
+
+  return (
+    <>
+      <GenericTable rows={rows} columns={columns} />
+      {open === true && (
+        <CustomModal
+          // maxWidth="450px"
+          showFullWidth={false}
+          showDivider={true}
+          showCloseBtn={true}
+          title={"Personal Details"}
+          isOpen={open}
+          handleClose={() => setOpen((prev: boolean) => !prev)}
+          children={<ProfileViewGlobal userData={userDataLocal} />}
+        />
+      )}
+    </>
+  );
 }
 
-export default AdminUserTables
+export default AdminUserTables;
