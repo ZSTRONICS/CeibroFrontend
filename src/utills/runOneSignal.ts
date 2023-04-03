2595172431f169d53a4e44863107908ad7d450ba
Talha Mubeen <33393512+TalhaMubeen@users.noreply.github.com>
2023-04-03 12:55:47 +0500

    oneSignal updated

--- src/utills/runOneSignal.ts
+++ src/utills/runOneSignal.ts
@@ -1,8 +1,10 @@
 import OneSignal from 'react-onesignal';
 
 export default async function runOneSignal() {
-  await OneSignal.init({ appId: '7ac7a441-9500-4f47-b370-c523db13de03',
-  safari_web_id: 'web.onesignal.auto.4d68c5e3-f56f-4052-bfb7-4c135295bfe6',
+  // await OneSignal.init({ appId: '7ac7a441-9500-4f47-b370-c523db13de03',
+  // safari_web_id: 'web.onesignal.auto.4d68c5e3-f56f-4052-bfb7-4c135295bfe6',
+  await OneSignal.init({ appId: '6b882cdf-7f71-46a0-a458-cda681fe770c',
+  safari_web_id: 'web.onesignal.auto.4cc30974-98f9-47ba-8e02-4635d2d477f2',
   allowLocalhostAsSecureOrigin: true,
   requiresUserPrivacyConsent: false,
   autoResubscribe: true
