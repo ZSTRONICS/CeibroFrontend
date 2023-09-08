86760ff2be50bd878385d9f6bf955b80754b4d24
Muhammad-Ahsan-Riaz <ahsanriaz@zstronics.com>
2023-09-08 16:06:15 +0500

    added some missing code

--- src/components/Tasks/Create-Task/CreateNewTask.tsx
+++ src/components/Tasks/Create-Task/CreateNewTask.tsx
@@ -13,7 +13,7 @@ import FileBox from "components/Utills/FileBox";
 import ImageBox from "components/Utills/ImageBox";
 import UserDropDown from "components/Utills/UserDropdown";
 import { isEmpty } from "lodash";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import {
@@ -52,6 +52,7 @@ var initialValues = {
 
 function CreateNewTask() {
   const [toggle, setToggle] = useState<boolean>(false);
+  const isRenderEffect = useRef<any>(false);
   const [isSubmit, setIsSubmit] = useState<boolean>(false);
   const [selectedImages, setSelectedImages] = useState<File[]>([]);
   const [selectedDocuments, setSelectedDocuments] = useState<File[]>([]);
@@ -70,20 +71,25 @@ function CreateNewTask() {
     (state: RootState) => state.user
   );
   const { user } = useSelector((state: RootState) => state.auth);
-  const tasks = useSelector((store: RootState) => store.task);
-  const projects = useSelector((store: RootState) => store.project);
-  const { allProjects } = projects;
-  const { Topics } = tasks;
+  const Topics = useSelector((state: RootState) => state.task.Topics);
+  const allProjects = useSelector(
+    (state: RootState) => state.project.allProjects
+  );
   const windowClose = window.getSelection();
 
   useEffect(() => {
+    if (!isRenderEffect.current) {
     dispatch(taskActions.getAllTopic());
+      if (allProjects.length === 0) {
     dispatch(getAllProjects());
-    // const payload = {
-    //   other: { userId: user._id },
-    // };
+      }
     userAllContacts.length < 1 && dispatch(userApiAction.getUserContacts());
-    recentUserContact.length < 1 && dispatch(userApiAction.getRecentContacts());
+      recentUserContact.length < 1 &&
+        dispatch(userApiAction.getRecentContacts());
+    }
+    return () => {
+      isRenderEffect.current = true;
+    };
   }, []);
 
   useEffect(() => {
