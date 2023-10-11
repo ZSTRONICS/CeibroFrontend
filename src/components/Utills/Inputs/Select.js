f4ef8b189469fe74ae3ce0c350ea54c67fc2e685
Ali Akbar <115136332+aakbar2022@users.noreply.github.com>
2023-10-11 21:32:33 +0500

    updated minifest and css styles font

--- src/components/Utills/Inputs/Select.js
+++ src/components/Utills/Inputs/Select.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 
 import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import NativeSelect from "@material-ui/core/NativeSelect";
 import InputBase from "@material-ui/core/InputBase";
+import NativeSelect from "@material-ui/core/NativeSelect";
 
 const BootstrapInput = withStyles((theme) => ({
   root: {
@@ -23,18 +21,7 @@ const BootstrapInput = withStyles((theme) => ({
     padding: "10px 26px 10px 12px",
     transition: theme.transitions.create(["border-color", "box-shadow"]),
     // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
+    fontFamily: "Inter",
     "&:focus": {
       borderRadius: 4,
       borderColor: "#80bdff",
